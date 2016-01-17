178bf8ee8ca5dc00aecc4995c9f054e3ebb3b3c9
Thomas Dippel <thomas@dippel.dk>
2016-01-17 23:24:54 +0100

    Simple implementation of users

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,29 @@
+# defmodule Rumbl.Repo do
+#   use Ecto.Repo, otp_app: :rumbl
+# end
+
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Thomas", username: "dipth", password: "qwerty"},
+     %Rumbl.User{id: "2", name: "Torben", username: "TC", password: "qwerty"},
+     %Rumbl.User{id: "3", name: "Bruno", username: "Naxx", password: "qwerty"}]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map ->
+      map.id == id
+    end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} ->
+        Map.get(map, key) == val end)
+    end
+  end
 end
