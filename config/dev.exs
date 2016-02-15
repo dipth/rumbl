2f5cbb92e8f2a97d15268ec4614cb19a6795dad9
Thomas Dippel <thomas@dippel.dk>
2016-02-15 23:14:59 +0100

    Implement an info system

--- config/dev.exs
+++ config/dev.exs
@@ -40,3 +40,7 @@ config :rumbl, Rumbl.Repo,
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
+
+# Finally import the config/dev.secret.exs
+# which should be versioned separately.
+import_config "dev.secret.exs"
